0960d7316931f265585bc75d417da87e368665fa
github username <github email address>
2019-10-30 16:31:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+#require 'pry'
+class School
+    attr_accessor :roster, :name
+
+    def initialize(school_name)
+        @name = school_name
+        @roster = {} 
+        puts @roster
+    end
+    
+    def add_student(name, grade)
+        if @roster.key?(grade)
+            #roster has a grade key, add to it
+            @roster[grade].push(name)
+        else
+             #roster does not have the grade key, create one
+             @roster[grade] = [name]
+        end
+     end
+
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        sorted_hash = {}
+        roster.each{|grade, name|
+        sorted_hash[grade] = name.sort
+        #binding.pry
+    }
+        sorted_hash
+    end
+end
